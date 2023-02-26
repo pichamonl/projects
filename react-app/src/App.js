e603dfa5d1a64cc897eddde655d71a0e1c0fc534
pichamonl <pichamonl@users.noreply.github.com>
2023-02-26 18:17:50 +0900

    (Update) Basic Expense

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    { id: 'e1', title: 'Lunch', amount: 1200, date: new Date(2023, 0, 22) },
+    { id: 'e2', title: 'Cake', amount: 2200, date: new Date(2023, 1, 24) },
+    { id: 'e3', title: 'Toothpaste', amount: 2200, date: new Date(2023, 1, 26) },
+    { id: 'e4', title: 'Soap', amount: 110, date: new Date(2023, 1, 3) },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get started!</h2>
+      {expenses.map(expense => (
+        <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}></ExpenseItem>
+      ))}
     </div>
   );
 }
